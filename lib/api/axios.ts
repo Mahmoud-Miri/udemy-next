03ac96518f65b39fbb96fd096ab5ee4a69aecaa0
Mahmoud Miri <sm_miri83@yahoo.com>
2023-03-05 02:41:31 +0000

    - dynamic baseUrl

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import getConfig from "next/config";
 
 const instance = axios.create({
-  baseURL: "https://mahmoud-miri.vercel.app",
+  baseURL: getConfig().publicRuntimeConfig.PUBLIC_API_BASE_URL,
 });
 
 instance.interceptors.request.use(
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    PUBLIC_API_BASE_URL: process.env.PUBLIC_API_BASE_URL,
+  },
   serverRuntimeConfig: {
     POEDITOR_API_TOKEN: process.env.POEDITOR_API_TOKEN,
     POEDITOR_PROJECT_ID: process.env.POEDITOR_PROJECT_ID,
